862562c8e7e9cf2a289e9a790089e38286c34b80
Edouard Coissy <coissy@treibauf.ch>
2022-09-29 17:44:26 +0200

    Initial commit

---
+++ start.js
@@ -0,0 +1,12 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  if (ns.getHostname() !== "home") {
+    throw new Exception("Run the script from home");
+  }
+
+  await ns.wget(
+    `https://raw.githubusercontent.com/moriakaice/bitburner/master/src/initHacking.js?ts=${new Date().getTime()}`,
+    "initHacking.js"
+  );
+  ns.spawn("initHacking.js", 1);
+}
